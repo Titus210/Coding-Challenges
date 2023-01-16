ed71a6061123065254bcbd8fa8b00ecd9af6d5c3
Walter Banda <walterkaimurima@gmail.com>
2023-01-16 20:00:58 +0300

    📖 Challenge problem & Description

--- leetCode/runningSum.java
+++ leetCode/runningSum.java
@@ -1,3 +1,23 @@
+/**
+ * ## 💻 Running Sum of 1d Array
+ * Leetcode daily study practice, you can find it
+ * [here](https://leetcode.com/problems/running-sum-of-1d-array/?envType=study-plan&id=level-1)
+ * 
+ * ### 📖 Challenge
+ * Given an array nums. We define a running sum of an array as `runningSum[i] =
+ * sum(nums[0]…nums[i])`.
+ * 
+ * Return the running sum of nums.
+ * 
+ * Example 1:
+ * 
+ * ```
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ * ```
+ */
+
 class Solution {
     public int[] runningSum(int[] nums) {
         /** Array to hold result of Running Sum */
