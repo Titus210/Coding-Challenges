b307a7db45762db000a27896ac17b8734d522216
Walter Banda <walterkaimurima@gmail.com>
2023-01-16 14:36:03 +0300

    💻 Today Leetcode challenges

---
+++ leetCode/runningSum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+    }
+}
+
+class Main {
+    public int main(){
+        Solution sln = new Solution();
+        sln.runningSums()
+    }
+}
