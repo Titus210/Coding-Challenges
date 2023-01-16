d58f5bce4c22cc9690d2e39caa18709aad9ae26b
Walter Banda <walterkaimurima@gmail.com>
2023-01-16 14:50:26 +0300

    ⚙️ Pseudocode of solution on runningSum java challenge

--- leetCode/runningSum.java
+++ leetCode/runningSum.java
@@ -1,11 +1,27 @@
 class Solution {
     public int[] runningSum(int[] nums) {
+        /** Array to hold result of Running Sum */
+        int[] res = {};
+
+        /**
+         * Iterated over the array adding the number in the current index with the next
+         * number and adding the sum into the [res] array
+         */
+        for (int i = 0; i < nums.length; i++) {
+        }
+
+        return res;
     }
 }
 
+/**
+ * Responsible for testing and running the solution
+ * Has two methods, [main] - runs the result and [getInput] - gets Input from
+ * user
+ */
 class Main {
-    public int main(){
+    public int main() {
         Solution sln = new Solution();
-        sln.runningSums()
+        sln.runningSum();
     }
 }
