edc8763224ed1bb72c09fdec87db73d81e525b29
Walter Banda <walterkaimurima@gmail.com>
2023-01-17 21:00:37 +0300

    ⚙️ Hackerrank solution template

--- hackarank/arithmeticOperators.js
+++ hackarank/arithmeticOperators.js
@@ -21,3 +21,43 @@
 // The area of the rectangle is ***`length X width = 3 X 4.5 = 13.5`***.
 // The perimeter of the rectangle is
 // ***`2 . (length + width) = 2 . (3 + 4.5) = 15`***.
+
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
+function getArea(length, width) {
+  let area;
+  // Write your code here
+
+  return area;
+}
+
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
+function getPerimeter(length, width) {
+  let perimeter;
+  // Write your code here
+
+  return perimeter;
+}
+
+function main() {
+  const length = +readLine();
+  const width = +readLine();
+
+  console.log(getArea(length, width));
+  console.log(getPerimeter(length, width));
+}
+
+main();
