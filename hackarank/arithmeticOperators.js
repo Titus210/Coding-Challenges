7871f79efd5f3731745ccd6ffd0970023bc62bb7
Walter Banda <walterkaimurima@gmail.com>
2023-01-17 20:37:08 +0300

    ⚙️ Hackerrank today first challenge description

---
+++ hackarank/arithmeticOperators.js
@@ -0,0 +1,23 @@
+// ## Objective
+// In this challenge, we practice using arithmetic operators. Check out the attached tutorial for resources.
+
+// ## Task
+// Complete the following functions in the editor below:
+//  1. `getArea(length, width)`: Calculate and return the area of a rectangle having sides ***`length`*** and  ***`width`***.
+//  2. `getPerimeter(length, width)`: Calculate and return the perimeter of a rectangle having sides ***`length`*** and  ***`width`***.
+
+// ### Sample Input 0
+// ```
+// 3
+// 4.5
+// ```
+// ### Sample Output 0
+// ```
+// 13.5
+// 15
+// ```
+
+// ### Explanation 0
+// The area of the rectangle is ***`length X width = 3 X 4.5 = 13.5`***.
+// The perimeter of the rectangle is
+// ***`2 . (length + width) = 2 . (3 + 4.5) = 15`***.
