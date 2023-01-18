f61fb816ed838fdd828eed341696d65313762642
Walter Banda <walterkaimurima@gmail.com>
2023-01-18 16:06:02 +0300

    ⚙️ Hackerrank todays challenge

---
+++ hackarank/vowelsConstonants.js
@@ -0,0 +1,56 @@
+// ## Objective
+
+// In this challenge, we practice looping over the characters of string. Check out the attached tutorial for more details.
+
+// ## Task
+
+// 1. First, print each vowel in ***s*** on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in ***s***.
+// 2. Second, print each consonant(i.e., non - vowel) in ***s*** on a new line in the same order as it appeared in ***s***.
+
+// ## Function Description
+
+// Complete the vowelsAndConsonants function in the editor below.
+
+// vowelsAndConsonants has the following parameters:
+
+// - string s: the string to process
+
+// ## Prints
+
+//     - Print each vowel of *** s *** in order on a new line, then print each consonant in order on a new line.Return nothing.
+
+// ## Input Format
+
+// There is one line of input with the string ***s***.
+
+// ## Output Format
+
+// First, print each vowel in ***s*** on a new line (in the same order as they appeared in ***s***). Second, print each consonant (i.e., non-vowel) in ***s*** on a new line (in the same order as they appeared in ***s***).
+
+// ## Sample Input 0
+
+// ```
+// javascriptloops
+// ```
+
+// ## Sample Output 0
+
+// ```
+// a
+// a
+// i
+// o
+// o
+// j
+// v
+// s
+// c
+// r
+// p
+// t
+// l
+// p
+// s
+// ```
+
+
