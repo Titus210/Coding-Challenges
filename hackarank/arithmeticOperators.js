b92654834756c8363aadb99c59ff432a412a28ed
Walter Banda <walterkaimurima@gmail.com>
2023-01-18 15:54:15 +0300

    ⚙️ Hackerrank yesterday solution

--- hackarank/arithmeticOperators.js
+++ hackarank/arithmeticOperators.js
@@ -33,6 +33,7 @@
 function getArea(length, width) {
   let area;
   // Write your code here
+  area = length * width;
 
   return area;
 }
@@ -48,6 +49,7 @@ function getArea(length, width) {
 function getPerimeter(length, width) {
   let perimeter;
   // Write your code here
+  perimeter = 2 * (length + width);
 
   return perimeter;
 }
